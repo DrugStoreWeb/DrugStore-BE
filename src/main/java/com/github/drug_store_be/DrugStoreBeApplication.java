ca47ac06f0ed5230fc31690d7bc62214688352b8
soheeparklee <soheeparklee@gmail.com>
2024-05-17 17:27:49 +0100

    springboot init

---
+++ src/main/java/com/github/drug_store_be/DrugStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.github.drug_store_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugStoreBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugStoreBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/drug_store_be/DrugStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.drug_store_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugStoreBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
