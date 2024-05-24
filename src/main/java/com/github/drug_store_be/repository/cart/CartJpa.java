490fa6a5a9061e4b1d712d7f46fd9a517a1961b6
soheeparklee <soheeparklee@gmail.com>
2024-05-24 11:32:47 +0200

    feature: cart entity, JPA

--- src/main/java/com/github/drug_store_be/repository/cart/CartJpa.java
+++ src/main/java/com/github/drug_store_be/repository/cart/CartJpa.java
@@ -1,8 +1,19 @@
 package com.github.drug_store_be.repository.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartJpa extends JpaRepository<Cart,Integer> {
+
+    @Query(
+            "SELECT c " +
+                    "FROM Cart c " +
+                    "WHERE c.user.userId =:userId "
+    )
+    List<Cart> findByUserId(Integer userId);
 }
