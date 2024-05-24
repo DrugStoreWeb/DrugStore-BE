4f143cc996bb5c2422610a95cdf9458d64157292
soheeparklee <soheeparklee@gmail.com>
2024-05-24 11:33:32 +0200

    feature: orderCouponDto

---
+++ src/main/java/com/github/drug_store_be/web/DTO/order/OrderCouponResponseDto.java
@@ -0,0 +1,16 @@
+package com.github.drug_store_be.web.DTO.order;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class OrderCouponResponseDto {
+    private String couponName;
+    private Integer couponDiscount;
+}
