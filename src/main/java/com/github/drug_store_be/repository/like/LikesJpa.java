e23dcbb8c59ae67a329669c651a9169a407278c8
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-05-26 19:11:04 +0900

    mainPage service logic

--- src/main/java/com/github/drug_store_be/repository/like/LikesJpa.java
+++ src/main/java/com/github/drug_store_be/repository/like/LikesJpa.java
@@ -1,5 +1,6 @@
 package com.github.drug_store_be.repository.like;
 
+import com.github.drug_store_be.repository.product.Product;
 import com.github.drug_store_be.repository.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,10 +11,15 @@ import java.util.Optional;
 
 @Repository
 public interface LikesJpa extends JpaRepository<Likes,Integer> {
-//    List<Likes> findByUserId(Optional<User> userId);
+
+//    @Query("SELECT l.product FROM Likes l GROUP BY l.product ORDER BY COUNT(l.likesId) DESC")
+//    static Product findTopByOrderByLikesDesc();
 
     @Query("SELECT l FROM Likes l WHERE l.user.userId = :userId AND l.product.productId = :productId")
     Likes findByUserIdAndProductId(int userId, Integer productId);
 
     @Query("SELECT SUM(l.likesId) FROM Likes l WHERE l.product.productId = :productId")
-    int findByProductId(Integer productId);}
+    int findByProductId(int productId);
+
+
+}
