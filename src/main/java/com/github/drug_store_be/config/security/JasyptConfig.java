4cdb56b532fe087fabf247cdd93299fc73f1bb8a
soheeparklee <soheeparklee@gmail.com>
2024-05-30 19:02:29 +0200

    feat: jasypt config

---
+++ src/main/java/com/github/drug_store_be/config/security/JasyptConfig.java
@@ -0,0 +1,32 @@
+package com.github.drug_store_be.config.security;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String PASSWORD_KEY;
+
+    @Bean(name= "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+        PooledPBEStringEncryptor encryptor= new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config= new SimpleStringPBEConfig();
+        config.setPassword(PASSWORD_KEY);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
