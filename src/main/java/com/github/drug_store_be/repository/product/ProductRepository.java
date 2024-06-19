935dd17147862a09e5200e7e3320b7bf06cad621
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-06-19 21:02:26 +0900

    fix likecount 정렬 쿼리문 에러 수정

--- src/main/java/com/github/drug_store_be/repository/product/ProductRepository.java
+++ src/main/java/com/github/drug_store_be/repository/product/ProductRepository.java
@@ -52,6 +52,7 @@ public interface ProductRepository extends JpaRepository<Product,Integer> {
     @Cacheable(value = "findProducts")
     @Query("SELECT p FROM Product p WHERE LENGTH(:keyword) >= 2 AND (p.brand LIKE %:keyword% OR p.productName LIKE %:keyword%)")
     List<Product> findByKeyword(@Param("keyword") String keyword);
+    @Query("SELECT COUNT(l) FROM Likes l JOIN l.product p WHERE p.productId = :productId")
     int countLikesByProductId(@Param("productId") Integer productId);
     @Query("SELECT CASE WHEN COUNT(l) > 0 THEN true ELSE false END " +
             "FROM Likes l WHERE l.user.userId = :userId AND l.product.productId = :productId")
