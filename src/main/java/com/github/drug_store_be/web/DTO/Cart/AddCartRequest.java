a6a58553fd68effc3e7db0421366be153658f4fa
kimdohee <ehgml1073@gmail.com>
2024-06-25 23:15:29 +0900

    변수명 변경

--- src/main/java/com/github/drug_store_be/web/DTO/Cart/AddCartRequest.java
+++ src/main/java/com/github/drug_store_be/web/DTO/Cart/AddCartRequest.java
@@ -1,5 +1,7 @@
 package com.github.drug_store_be.web.DTO.Cart;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AddCartRequest {
     private Integer productId;
     private Integer quantity;
