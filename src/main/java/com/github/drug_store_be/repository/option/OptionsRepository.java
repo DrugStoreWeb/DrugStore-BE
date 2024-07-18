059952832a4461d32a17ed874a7d44ba6334e583
soheeparklee <soheeparklee@gmail.com>
2024-07-18 17:47:54 +0200

    feat: option repository lock

--- src/main/java/com/github/drug_store_be/repository/option/OptionsRepository.java
+++ src/main/java/com/github/drug_store_be/repository/option/OptionsRepository.java
@@ -1,13 +1,16 @@
 package com.github.drug_store_be.repository.option;
 
 import com.github.drug_store_be.repository.product.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OptionsRepository extends JpaRepository<Options,Integer> {
@@ -20,4 +23,10 @@ public interface OptionsRepository extends JpaRepository<Options,Integer> {
 
     List<Options> findAllByProductProductId(Integer productId);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(
+            "SELECT o FROM Options o " +
+                    "WHERE o.optionsId = :optionId "
+    )
+    Optional<Options> findByIdWithLock(int optionId);
 }
